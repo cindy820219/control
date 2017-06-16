9fdee14d24026337303e5f90feb90fdfac03385e
cindy820219 <s100321035@mail1.ncnu.edu.tw>
2017-06-16 17:16:03 +0800

    a.py

--- a.py
+++ a.py
@@ -14,62 +14,47 @@ se.port = '/dev/ttyACM0'
 se.timeout = 0.5
 se.rtscts = 1
 
-se.open()                       # open ;port
+# open port
+se.open()                       
+
+def IDFunc(ID):
+    if ID == 'FB13':
+        Sensor = 1
+    else:
+        Sensor = 2
+    print("Sensor: ", Sensor)
 
 while True:
-  response = se.readline()
-  print(str(response))
-ser.close()
-
-
-'''
-def reader():
-    while(True):
-        try:
-            se.open()                       # open ;port
-            
-            #~ print(se.portstr)            # confirm which port was really used
-                
-            #se.write(85)                   # send command string to meter
-            bytes=struct.unpack("i", struct.pack('i',0x55)) #0x55  85
-            #~ print("bytes: ", bytes)
-            #~ se.write(bytes)
-            
-            #data = se.readline()           # read data from meter 
-            data = se.read(5)               # read data from meter 
-
-            #~ print(list(data))            # 10進制
-            #~ print(hex(data[0]))          # 轉16進制
-            #~ print(len(data))             # 長度
-            
-            if len(data)>1:
-                print()
-                print()
-                print()
-
-                #~ 16 [0x7b 0x22 0x72 0x74 0x6e]
-                #~ 10 [123, 34, 114, 116, 110]
-                
-                print("d:", hex(data[0]), hex(data[1]),hex(data[2]),hex(data[3]), hex(data[4]))           # 16進制
-                print(list(data))           # 10進制
-                print("0:", data)
-
-                #~ while i <= 4:
-                    #~ print(hex(data[i]))
-                    #~ i += 1
-            else:
-                #~ print("null")
-                a=0
-                
-        except:
-            print("except")
-        else:
-            #~ print("done!")
-            a=0
-            
-        finally:
-            se.close() 
+    response = se.readline()
+    response = str(response)
+    if response != "b''":
+        #~ print(response)
         
+        ### CO2
+        if response[13:14] == 'c':
+            ### ID
+            ID = response[37:41]
+            IDFunc(ID)
+            
+            #~ print("CO2: ",response[63:69])
+            #~ int(int('0x034B', 16))
+            CO2 = int(int(response[63:69], 16))
+            print("CO2:",CO2)
+        
+        ### Temp / Hum
+        else: 
+            ### ID
+            ID = response[36:40]
+            IDFunc(ID)
+            
+            
+            #~ print("Temp: ",response[63:69])
+            #~ print("Hume: ",response[80:86])
+            Temp = int(int(response[63:69], 16))
+            Hume = int(int(response[80:86], 16))
+            print("Temp:",Temp)
+            print("Hume:",Hume)
+        print("-----")
 
-reader()
-'''
+
+ser.close()
