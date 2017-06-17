2277cda29c2b0cd8681f5e25cd1ec4d3593835ff
cindy820219 <s100321035@mail1.ncnu.edu.tw>
2017-06-17 19:01:44 +0800

    ani

--- a.py
+++ a.py
@@ -3,10 +3,19 @@
 ### diff yes.txt no.txt
 ### sudo python3 a.py
 
+### port
 import serial
 import struct
 import time                             # time immedita
 
+### draw 
+import matplotlib.pyplot as plt
+from tkinter import *
+import numpy as np
+import matplotlib.animation as animation
+from matplotlib import style
+
+### port
 se = serial.Serial()
 se.baudrate = 38400
 se.bytesize = 8
@@ -14,38 +23,58 @@ se.stopbits = 1
 se.port = '/dev/ttyACM0'
 se.timeout = 0.5
 se.rtscts = 1
-
-
 se.open()                               # open port
 
+### draw
+#~ plt.title('CO2 real time')        # give plot a title
+#~ plt.xlabel('x axis / Real time')    # make axis labels
+#~ plt.ylabel('y axis / ppm' )
+#~ plt.axis([0, 1000000, 0, 2500])          # [x,x,y,y]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+x = 0
+
+### ID
 def IDFunc(ID):
-    print (time.strftime("%H:%M:%S"))
     if ID == 'FB13':
         Sensor = 1
     else:
         Sensor = 2
-    print("Sensor: ", Sensor)
+    return Sensor
 
 while True:
     response = se.readline()
-
     if response != "b''":
+        tm = time.strftime("%H:%M:%S")
         response = str(response)
-        #~ print(response)
-        
         ### CO2 
         if response[13:14] == 'c':
+            x = x + 1
             ### ID
             ID = response[37:41]
-            IDFunc(ID)
-            
+            Sensor = IDFunc(ID)
+            print(Sensor)
+                
             #~ print("CO2: ",response[63:69])
             #~ int(int('0x034B', 16))
             CO2 = int(int(response[63:69], 16))
             print("CO2:",CO2)
-        
-        print("-----")
-        
+            print("-----")
+                                  
+            if Sensor == 1:
+                f = open("note1.txt", 'a')
+            else: 
+                f = open("note2.txt", 'a')
+            f.write(str(x)+ ',' + str(tm) + ',' + str(CO2) + '\n') 
+            f.close()
+            
+
+
+
+
+
         ### Temp / Hum
         #~ else: 
             #~ ### ID
@@ -62,4 +91,4 @@ while True:
         #~ print("-----")
 
 
-ser.close()
+#~ ser.close()
--- draw.py
+++ draw.py
@@ -2,55 +2,44 @@ import matplotlib.pyplot as plt
 from tkinter import *
 import numpy as np
 
-plt.title('CO2 real time')        # give plot a title
-plt.xlabel('x axis / Real time')    # make axis labels
-plt.ylabel('y axis / ppm' )
+import matplotlib.animation as animation
 
-plt.axis([0, 10, 0, 2000])          # [x,x,y,y]
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
 
-
-#~ filea = open("try.txt", "r")
-#~ for lines in filea:  
-    #~ print(str(lines[63:67]))
-#~ filea.close()
-
-#~ str = "{\"rtn_id\":\"co2_data\",\"nwk_addr\":\"0xFA8F\",\"phy_data\":[{\"co2\":\"0x054D\"}]}"
-#~ print(str[63:67])
-
-a = int(int('0x065C', 16))
-b = int(int('0x053C', 16))
-c = int(int('0x0344', 16))
-d = int(int('0x039C', 16))
-e = int(int('0x0340', 16))
-f = int(int('0x038E', 16))
-g = int(int('0x0337', 16))
-h = int(int('0x0398', 16))
-i = int(int('0x033E', 16))
-j = int(int('0x038B', 16))
-k = int(int('0x034B', 16))
-l = int(int('0x0397', 16))
-
-print(a,b,c,d,e,f,g,h,i,j,k,l)
-
-
-### data 1 red
-### x ---> real time
-### y ---> ppm
-x1 = [1,2,3,4,5]
-y1 = [a,c,e,g,i]
-
-### data 2 green
-x2 = [1,2,3,4,5]
-y2 = [b,d,f,h,j]
-
-for i in range(5):
-    plt.scatter(x1[i], y1[i])
-    plt.scatter(x2[i], y2[i])
+def animate(i):
+    graph_data = open('note1.txt','r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            line, x, y = line.split(',')
+            xs.append(line)
+            ys.append(y)
+            
+    ax1.clear()
+    ax1.plot(xs, ys)
     
-    #~ plt.plot(x1, y1, 'r')               # use pylab to plot x and y
-    #~ plt.plot(x2, y2, 'g')
-    plt.pause(0.5)
+    graph_data1 = open('note2.txt','r').read()
+    lines = graph_data1.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            line, x, y = line.split(',')
+            xs.append(line)
+            ys.append(y)
+            
+    ax2.clear()
+    ax2.plot(xs, ys)
+    
+    plt.xlabel('x axis / Real time')    # make axis labels
+    plt.ylabel('y axis / ppm' )
+    
+    plt.title('CO2 real time')        # give plot a title
 
-#~ plt.show()                          # show the plot on the screen
-while True:
-    plt.pause(0.5)
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
