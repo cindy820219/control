a2d47153f72f8a7e34c082451ac5d6f29e9384af
cindy820219 <s100321035@mail1.ncnu.edu.tw>
2017-06-17 20:13:23 +0800

    finish: 	real time is finished! next step: 	rolling view

--- a.py
+++ a.py
@@ -25,16 +25,8 @@ se.timeout = 0.5
 se.rtscts = 1
 se.open()                               # open port
 
-### draw
-#~ plt.title('CO2 real time')        # give plot a title
-#~ plt.xlabel('x axis / Real time')    # make axis labels
-#~ plt.ylabel('y axis / ppm' )
-#~ plt.axis([0, 1000000, 0, 2500])          # [x,x,y,y]
-
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-
-x = 0
+x1 = 0
+x2 = 0
 
 ### ID
 def IDFunc(ID):
@@ -51,29 +43,25 @@ while True:
         response = str(response)
         ### CO2 
         if response[13:14] == 'c':
-            x = x + 1
             ### ID
             ID = response[37:41]
             Sensor = IDFunc(ID)
-            print(Sensor)
-                
+            
             #~ print("CO2: ",response[63:69])
             #~ int(int('0x034B', 16))
             CO2 = int(int(response[63:69], 16))
-            print("CO2:",CO2)
-            print("-----")
-                                  
+            print("Sensor:", Sensor, "; CO2:",CO2)
+
             if Sensor == 1:
+                x1 = x1 + 1
                 f = open("note1.txt", 'a')
+                f.write(str(x1)+ ',' + str(tm) + ',' + str(CO2) + '\n') 
             else: 
+                x2 = x2 + 1
                 f = open("note2.txt", 'a')
-            f.write(str(x)+ ',' + str(tm) + ',' + str(CO2) + '\n') 
+                f.write(str(x2)+ ',' + str(tm) + ',' + str(CO2) + '\n') 
             f.close()
             
-
-
-
-
 
         ### Temp / Hum
         #~ else: 
@@ -89,6 +77,3 @@ while True:
             #~ print("Temp:",Temp)
             #~ print("Hume:",Hume)
         #~ print("-----")
-
-
-#~ ser.close()
--- draw.py
+++ draw.py
@@ -1,44 +1,50 @@
 import matplotlib.pyplot as plt
 from tkinter import *
 import numpy as np
-
 import matplotlib.animation as animation
 
 fig = plt.figure()
 ax1 = fig.add_subplot(211)
+plt.title('CO2 1')        # give plot a title
+#~ plt.xlabel('Real time')    # make axis labels
+plt.ylabel('ppm' )
+
 ax2 = fig.add_subplot(212)
+plt.title('CO2 2')        # give plot a title
+plt.xlabel('Real time')    # make axis labels
+plt.ylabel('ppm' )
 
 def animate(i):
-    graph_data = open('note1.txt','r').read()
-    lines = graph_data.split('\n')
-    xs = []
-    ys = []
-    for line in lines:
-        if len(line) > 1:
-            line, x, y = line.split(',')
-            xs.append(line)
-            ys.append(y)
-            
-    ax1.clear()
-    ax1.plot(xs, ys)
-    
-    graph_data1 = open('note2.txt','r').read()
+    graph_data1 = open('note1.txt','r').read()
     lines = graph_data1.split('\n')
-    xs = []
-    ys = []
+    x1 = []
+    y1 = []
     for line in lines:
         if len(line) > 1:
-            line, x, y = line.split(',')
-            xs.append(line)
-            ys.append(y)
+            line_1, time_1, y_1 = line.split(',')
+            x1.append(line_1)
+            y1.append(y_1)
+            ax1.text(line_1, y_1, time_1+'\n'+y_1)
+
+    graph_data2 = open('note2.txt','r').read()
+    lines = graph_data2.split('\n')
+    x2 = []
+    y2 = []
+    for line in lines:
+        if len(line) > 1:
+            line_2, time_2, y_2 = line.split(',')
+            x2.append(line_2)
+            y2.append(y_2)
+            ax2.text(line_2, y_2, time_2+'\n'+y_2)
             
-    ax2.clear()
-    ax2.plot(xs, ys)
+    #~ ax1.clear()
+    ax1.plot(x1, y1)
+    #~ ax2.clear()
+    ax2.plot(x2, y2)
     
-    plt.xlabel('x axis / Real time')    # make axis labels
-    plt.ylabel('y axis / ppm' )
-    
-    plt.title('CO2 real time')        # give plot a title
+    #~ plt.xlabel('x axis / Real time')    # make axis labels
+    #~ plt.ylabel('y axis / ppm' )
+    #~ plt.title('CO2 real time')        # give plot a title
 
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
